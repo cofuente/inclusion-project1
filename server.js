332d77cb814f51f609f0204b2b88ccf936f1ae6b
cofuente <contact@cofuente.io>
2020-02-07 20:34:42 -0500

    removed semis

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const reservationsRouter = require('./routes/reservations');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const reservationsRouter = require('./routes/reservations')
 
 // serve the homepage from here
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // handle api calls from here
-app.use('/reservations', reservationsRouter);
+app.use('/reservations', reservationsRouter)
 
 app.listen(port, () => {
-  console.log(`Server app listening on port ${port}!`);
-});
+  console.log(`Server app listening on port ${port}!`)
+})
