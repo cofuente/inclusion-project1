a864eea01fd78095b5f461868e3370f03d611b34
cofuente <contact@cofuente.io>
2020-02-07 20:19:52 -0500

    removed semis

--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const { Reservation } = require('../../models');
+const express = require('express')
+const router = express.Router()
+const { Reservation } = require('../../models')
 
 router.get('/', async (req,res) => {
-  res.json(await Reservation.all());
-});
+  res.json(await Reservation.all())
+})
 
-module.exports = router;
+module.exports = router
