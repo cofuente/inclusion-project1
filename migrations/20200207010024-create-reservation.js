cf905737dfaca82802449031252928856524f33a
cofuente <contact@cofuente.io>
2020-02-07 20:31:45 -0500

    removed semis

--- migrations/20200207010024-create-reservation.js
+++ migrations/20200207010024-create-reservation.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Reservations', {
@@ -22,9 +22,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Reservations');
+    return queryInterface.dropTable('Reservations')
   }
-};
+}
--- models/reservation.js
+++ models/reservation.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const SequelizeReservation = sequelize.define('Reservation', {
     name: DataTypes.STRING,
     slot: DataTypes.DATE
-  }, {});
+  }, {})
 
   class Reservation extends SequelizeReservation {
     static async all(){
-      return await this.findAll();
+      return await this.findAll()
     }
   }
 
-  return Reservation;
-};
+  return Reservation
+}
