1595e7c5fc3d5bc6f1e13ec3917a72715ccc992c
cofuente <contact@cofuente.io>
2020-02-10 01:05:10 -0500

    holy smokes look at the time

--- migrations/20200207010024-create-reservation.js
+++ migrations/20200207010024-create-reservation.js
@@ -17,12 +17,25 @@ module.exports = {
         type: Sequelize.INTEGER, // 18 half hour slots, 0-17
         validate: {
           isInt: true,
-          isIn: [[0,1,2,3,4,5,6,6,8,9,10,11,12,13,14,15,16,17]]
+          min: 0,
+          max: 17
         }
       },
+      // day: {
+      //   allowNull: false,
+      //   validate: {
+      //     isDate: true,
+      //     isAfter: new Date()
+      //   }
+      // },
       table:{
         allowNull: false,
-        type: Sequelize.ENUM('1','2','3') // 10 tables
+        type: Sequelize.INTEGER, // 10 tables 1-10
+        validate: {
+          isInt: true,
+          min: 1,
+          max: 10
+        }
       },
       createdAt: {
         allowNull: false,
--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,30 @@
 let resData = reservationFetch.then((data) => {
     console.log('Success:', data)
 })
+
+document.getElementById('slot').onchange = function (event) {
+  console.log(event.target.value)
+}
+
+document.getElementById('table').onchange = function (event) {
+  console.log(event.target.value)
+}
+
+function nameChange(event) {
+  console.log(event.target.value)
+}
+
+document.getElementById('name').addEventListener('change', nameChange)
+
+
+document.getElementById('day').addEventListener('change', nameChange)
+
+// document.getElementById('name').addEventListener('submit', submitFunction)
+
+
+function submitFunction(event) {
+  event.preventDefault()
+//   const msg = document.getElementById('time').value
+  console.log(event.target.value)
+  console.log(proposedReservation)
+}
