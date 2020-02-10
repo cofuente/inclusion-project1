57beab33e44dee46459a3af9fb37ea6079e6e79a
cofuente <contact@cofuente.io>
2020-02-10 01:33:36 -0500

    ok really stopping now

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
 // jest won't error out on DOM calls.
-// console.log(document.querySelector('p').innerHTML)
+// DOM & fetch
 
-let resData = reservationFetch.then((data) => {
-    console.log('Success:', data)
+const getReservations = fetch("http://localhost:3000/reservations")
+.then((response) => response.json()).then((data) => {
+    console.log('Successfully loaded reservations:', data)
 })
 
 document.getElementById('slot').onchange = function (event) {
@@ -13,21 +14,9 @@ document.getElementById('table').onchange = function (event) {
   console.log(event.target.value)
 }
 
-function nameChange(event) {
-  console.log(event.target.value)
-}
-
 document.getElementById('name').addEventListener('change', nameChange)
 
 
 document.getElementById('day').addEventListener('change', nameChange)
 
-// document.getElementById('name').addEventListener('submit', submitFunction)
-
-
-function submitFunction(event) {
-  event.preventDefault()
-//   const msg = document.getElementById('time').value
-  console.log(event.target.value)
-  console.log(proposedReservation)
-}
+document.getElementById('form').addEventListener('submit', submitFunction)
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,13 @@
+// process Data
 // // define your functions like this so they're testable
 // function sum (num1, num2) {
 //   return num1 + num2
 // }
-const reservationFetch = fetch("http://localhost:3000/reservations")
-.then((response) => response.json())
+function submitFunction(event) {
+    event.preventDefault()
+    console.log('----->', event)
+}
+
+function nameChange(event) {
+    console.log(event.target.value)
+}
